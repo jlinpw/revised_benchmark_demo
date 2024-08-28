8aecc5eac9136125e750d94f66cd78b58009bf1c
Joey Lin <jlin@parallelworks.com>
2024-08-28 16:59:03 -0400

    scp output files back to cluster

--- run_batch.sh
+++ run_batch.sh
@@ -35,5 +35,9 @@ done
 
 echo "batch job done!"
 
-# copy the job output file back to the workflow run dir
+# copy the job output files back to the workflow run dir
 scp ${WFP_whost}:${HOME}/slurm_job_${jobid}.out ${jobdir}
+scp ${WFP_whost}:${HOME}/test_build_${jobid}.txt ${jobdir}
+scp ${WFP_whost}:${HOME}/alltoall_${jobid}.txt ${jobdir}
+scp ${WFP_whost}:${HOME}/${jsource}_ior_${jobid}.txt ${jobdir}
+scp ${WFP_whost}:${HOME}/${jsource}_mdtest_${jobid}.txt ${jobdir}
